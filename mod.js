d0a3b77064ee2af3b91bc29eb05b263872103f28
emdsa2 <1260556316@qq.com>
2024-07-29 11:14:11 +0800

    回退

--- mod.js
+++ mod.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         BC 服装拓展 无🍔版
+// @name         BC 服装拓展
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  服装拓展 无🍔版
+// @description  服装拓展
 // @author       Luzi
 // @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
@@ -16,8 +16,8 @@
         "use strict"; const o = "1.2.0"; function e(o) { alert("Mod ERROR:\n" + o); const e = new Error(o); throw console.error(e), e } const t = new TextEncoder; function n(o) { return !!o && "object" == typeof o && !Array.isArray(o) } function r(o) { const e = new Set; return o.filter((o => !e.has(o) && e.add(o))) } const i = new Map, a = new Set; function c(o) { a.has(o) || (a.add(o), console.warn(o)) } function s(o) { const e = [], t = new Map, n = new Set; for (const r of f.values()) { const i = r.patching.get(o.name); if (i) { e.push(...i.hooks); for (const [e, a] of i.patches.entries()) t.has(e) && t.get(e) !== a && c(`ModSDK: Mod '${r.name}' is patching function ${o.name} with same pattern that is already applied by different mod, but with different pattern:\nPattern:\n${e}\nPatch1:\n${t.get(e) || ""}\nPatch2:\n${a}`), t.set(e, a), n.add(r.name) } } e.sort(((o, e) => e.priority - o.priority)); const r = function (o, e) { if (0 === e.size) return o; let t = o.toString().replaceAll("\r\n", "\n"); for (const [n, r] of e.entries()) t.includes(n) || c(`ModSDK: Patching ${o.name}: Patch ${n} not applied`), t = t.replaceAll(n, r); return (0, eval)(`(${t})`) }(o.original, t); let i = function (e) { var t, i; const a = null === (i = (t = m.errorReporterHooks).hookChainExit) || void 0 === i ? void 0 : i.call(t, o.name, n), c = r.apply(this, e); return null == a || a(), c }; for (let t = e.length - 1; t >= 0; t--) { const n = e[t], r = i; i = function (e) { var t, i; const a = null === (i = (t = m.errorReporterHooks).hookEnter) || void 0 === i ? void 0 : i.call(t, o.name, n.mod), c = n.hook.apply(this, [e, o => { if (1 !== arguments.length || !Array.isArray(e)) throw new Error(`Mod ${n.mod} failed to call next hook: Expected args to be array, got ${typeof o}`); return r.call(this, o) }]); return null == a || a(), c } } return { hooks: e, patches: t, patchesSources: n, enter: i, final: r } } function l(o, e = !1) { let r = i.get(o); if (r) e && (r.precomputed = s(r)); else { let e = window; const a = o.split("."); for (let t = 0; t < a.length - 1; t++)if (e = e[a[t]], !n(e)) throw new Error(`ModSDK: Function ${o} to be patched not found; ${a.slice(0, t + 1).join(".")} is not object`); const c = e[a[a.length - 1]]; if ("function" != typeof c) throw new Error(`ModSDK: Function ${o} to be patched not found`); const l = function (o) { let e = -1; for (const n of t.encode(o)) { let o = 255 & (e ^ n); for (let e = 0; e < 8; e++)o = 1 & o ? -306674912 ^ o >>> 1 : o >>> 1; e = e >>> 8 ^ o } return ((-1 ^ e) >>> 0).toString(16).padStart(8, "0").toUpperCase() }(c.toString().replaceAll("\r\n", "\n")), d = { name: o, original: c, originalHash: l }; r = Object.assign(Object.assign({}, d), { precomputed: s(d), router: () => { }, context: e, contextProperty: a[a.length - 1] }), r.router = function (o) { return function (...e) { return o.precomputed.enter.apply(this, [e]) } }(r), i.set(o, r), e[r.contextProperty] = r.router } return r } function d() { for (const o of i.values()) o.precomputed = s(o) } function p() { const o = new Map; for (const [e, t] of i) o.set(e, { name: e, original: t.original, originalHash: t.originalHash, sdkEntrypoint: t.router, currentEntrypoint: t.context[t.contextProperty], hookedByMods: r(t.precomputed.hooks.map((o => o.mod))), patchedByMods: Array.from(t.precomputed.patchesSources) }); return o } const f = new Map; function u(o) { f.get(o.name) !== o && e(`Failed to unload mod '${o.name}': Not registered`), f.delete(o.name), o.loaded = !1, d() } function g(o, t) { o && "object" == typeof o || e("Failed to register mod: Expected info object, got " + typeof o), "string" == typeof o.name && o.name || e("Failed to register mod: Expected name to be non-empty string, got " + typeof o.name); let r = `'${o.name}'`; "string" == typeof o.fullName && o.fullName || e(`Failed to register mod ${r}: Expected fullName to be non-empty string, got ${typeof o.fullName}`), r = `'${o.fullName} (${o.name})'`, "string" != typeof o.version && e(`Failed to register mod ${r}: Expected version to be string, got ${typeof o.version}`), o.repository || (o.repository = void 0), void 0 !== o.repository && "string" != typeof o.repository && e(`Failed to register mod ${r}: Expected repository to be undefined or string, got ${typeof o.version}`), null == t && (t = {}), t && "object" == typeof t || e(`Failed to register mod ${r}: Expected options to be undefined or object, got ${typeof t}`); const i = !0 === t.allowReplace, a = f.get(o.name); a && (a.allowReplace && i || e(`Refusing to load mod ${r}: it is already loaded and doesn't allow being replaced.\nWas the mod loaded multiple times?`), u(a)); const c = o => { let e = g.patching.get(o.name); return e || (e = { hooks: [], patches: new Map }, g.patching.set(o.name, e)), e }, s = (o, t) => (...n) => { var i, a; const c = null === (a = (i = m.errorReporterHooks).apiEndpointEnter) || void 0 === a ? void 0 : a.call(i, o, g.name); g.loaded || e(`Mod ${r} attempted to call SDK function after being unloaded`); const s = t(...n); return null == c || c(), s }, p = { unload: s("unload", (() => u(g))), hookFunction: s("hookFunction", ((o, t, n) => { "string" == typeof o && o || e(`Mod ${r} failed to patch a function: Expected function name string, got ${typeof o}`); const i = l(o), a = c(i); "number" != typeof t && e(`Mod ${r} failed to hook function '${o}': Expected priority number, got ${typeof t}`), "function" != typeof n && e(`Mod ${r} failed to hook function '${o}': Expected hook function, got ${typeof n}`); const s = { mod: g.name, priority: t, hook: n }; return a.hooks.push(s), d(), () => { const o = a.hooks.indexOf(s); o >= 0 && (a.hooks.splice(o, 1), d()) } })), patchFunction: s("patchFunction", ((o, t) => { "string" == typeof o && o || e(`Mod ${r} failed to patch a function: Expected function name string, got ${typeof o}`); const i = l(o), a = c(i); n(t) || e(`Mod ${r} failed to patch function '${o}': Expected patches object, got ${typeof t}`); for (const [n, i] of Object.entries(t)) "string" == typeof i ? a.patches.set(n, i) : null === i ? a.patches.delete(n) : e(`Mod ${r} failed to patch function '${o}': Invalid format of patch '${n}'`); d() })), removePatches: s("removePatches", (o => { "string" == typeof o && o || e(`Mod ${r} failed to patch a function: Expected function name string, got ${typeof o}`); const t = l(o); c(t).patches.clear(), d() })), callOriginal: s("callOriginal", ((o, t, n) => { "string" == typeof o && o || e(`Mod ${r} failed to call a function: Expected function name string, got ${typeof o}`); const i = l(o); return Array.isArray(t) || e(`Mod ${r} failed to call a function: Expected args array, got ${typeof t}`), i.original.apply(null != n ? n : globalThis, t) })), getOriginalHash: s("getOriginalHash", (o => { "string" == typeof o && o || e(`Mod ${r} failed to get hash: Expected function name string, got ${typeof o}`); return l(o).originalHash })) }, g = { name: o.name, fullName: o.fullName, version: o.version, repository: o.repository, allowReplace: i, api: p, loaded: !0, patching: new Map }; return f.set(o.name, g), Object.freeze(p) } function h() { const o = []; for (const e of f.values()) o.push({ name: e.name, fullName: e.fullName, version: e.version, repository: e.repository }); return o } let m; const y = void 0 === window.bcModSdk ? window.bcModSdk = function () { const e = { version: o, apiVersion: 1, registerMod: g, getModsInfo: h, getPatchingInfo: p, errorReporterHooks: Object.seal({ apiEndpointEnter: null, hookEnter: null, hookChainExit: null }) }; return m = e, Object.freeze(e) }() : (n(window.bcModSdk) || e("Failed to init Mod SDK: Name already in use"), 1 !== window.bcModSdk.apiVersion && e(`Failed to init Mod SDK: Different version already loaded ('1.2.0' vs '${window.bcModSdk.version}')`), window.bcModSdk.version !== o && alert(`Mod SDK warning: Loading different but compatible versions ('1.2.0' vs '${window.bcModSdk.version}')\nOne of mods you are using is using an old version of SDK. It will work for now but please inform author to update`), window.bcModSdk); return "undefined" != typeof exports && (Object.defineProperty(exports, "__esModule", { value: !0 }), exports.default = y), y
     }();
 
-    const MOD_NAME = "服装拓展 无🍔版";
-    const MOD_FULL_NAME = "服装拓展 无🍔版";
+    const MOD_NAME = "服装拓展";
+    const MOD_FULL_NAME = "服装拓展";
     const MOD_VERSION = "0.2.0";
     const MOD_REPOSITORY = "https://github.com/emdsa2/-mod";
     const Luzi = "笨蛋Luzi";
@@ -2340,6 +2340,96 @@
         AssetAdd(G, asset, AssetFemale3DCGExtended);
     }
 
+    function addExtraOutfitsToAssets() {
+        // 需要执行相同操作的项的数组
+        const itemsToCopy = ["Cloth", "ClothAccessory", "ClothLower", "Panties", "Necklace", "Bra", "Hat", "Shoes", "HairAccessory3", "Mask", "Wings", "Gloves"];
+
+        // 循环遍历每个需要复制的项
+        itemsToCopy.forEach(itemName => {
+            // 找到对应项的索引位置
+            let itemIndex = AssetFemale3DCG.findIndex(A => A.Group === itemName);
+            if (itemIndex !== -1) { // 如果找到了对应项
+                // 复制对应项
+                let itemCopy = Object.assign({}, AssetFemale3DCG[itemIndex]); // 假设 AssetFemale3DCG 里的项是对象，如果是数组则使用 slice() 方法
+                itemCopy.Group = itemName + "_笨笨蛋Luzi"; // 修改复制的项的名称为原名称加上 "2"
+
+                // 获取复制项的 Asset 数组
+                let copiedAssets = itemCopy.Asset;
+                copiedAssets.forEach(asset => {
+                    // 给每个对象都加上 Random: false 属性（如果不存在的话）
+                    asset.Random = false;
+                });
+                AssetFemale3DCG.splice(itemIndex + 1, 0, itemCopy); // 在原索引位置之后插入复制的项
+            }
+        });
+
+        // 遍历 itemsToCopy 中的每一项
+        itemsToCopy.forEach(itemName => {
+            // 找到对应项的对象
+            const item = AssetFemale3DCGExtended[itemName];
+            if (item) { // 如果找到了对应项
+                // 复制对应项
+                const itemCopy = { ...item };
+                // 修改复制的项的名称为原名称加上 "2"
+                const newItemName = itemName + "_笨笨蛋Luzi";
+                itemCopy.Group = newItemName;
+
+                // 将修改后的项添加到原数组中
+                AssetFemale3DCGExtended[newItemName] = itemCopy;
+            }
+        });
+
+        // 提取 AssetFemale3DCG 中 Group 属性包含 '_笨笨蛋Luzi' 的对象存入新数组
+        const filteredAssetFemale3DCG = AssetFemale3DCG.filter(asset => asset.Group.includes('_笨笨蛋Luzi'));
+        // 输出结果
+        AssetLoad(filteredAssetFemale3DCG, "Female3DCG", AssetFemale3DCGExtended);
+    }
+
+    function addExtraOutfitsToAssets2() {
+        // 需要执行相同操作的项的数组
+        const itemsToCopy = ["Cloth", "ClothLower"];
+
+        // 循环遍历每个需要复制的项
+        itemsToCopy.forEach(itemName => {
+            // 找到对应项的索引位置
+            let itemIndex = AssetFemale3DCG.findIndex(A => A.Group === itemName);
+            if (itemIndex !== -1) { // 如果找到了对应项
+                // 复制对应项
+                let itemCopy = Object.assign({}, AssetFemale3DCG[itemIndex]); // 假设 AssetFemale3DCG 里的项是对象，如果是数组则使用 slice() 方法
+                itemCopy.Group = itemName + "_笨笨笨蛋Luzi2"; // 修改复制的项的名称为原名称加上 "2"
+
+                // 获取复制项的 Asset 数组
+                let copiedAssets = itemCopy.Asset;
+                copiedAssets.forEach(asset => {
+                    // 给每个对象都加上 Random: false 属性（如果不存在的话）
+                    asset.Random = false;
+                });
+                AssetFemale3DCG.splice(itemIndex + 1, 0, itemCopy); // 在原索引位置之后插入复制的项
+            }
+        });
+
+        // 遍历 itemsToCopy 中的每一项
+        itemsToCopy.forEach(itemName => {
+            // 找到对应项的对象
+            const item = AssetFemale3DCGExtended[itemName];
+            if (item) { // 如果找到了对应项
+                // 复制对应项
+                const itemCopy = { ...item };
+                // 修改复制的项的名称为原名称加上 "2"
+                const newItemName = itemName + "_笨笨笨蛋Luzi2";
+                itemCopy.Group = newItemName;
+
+                // 将修改后的项添加到原数组中
+                AssetFemale3DCGExtended[newItemName] = itemCopy;
+            }
+        });
+
+        // 提取 AssetFemale3DCG 中 Group 属性包含 '_笨笨蛋Luzi' 的对象存入新数组
+        const filteredAssetFemale3DCG = AssetFemale3DCG.filter(asset => asset.Group.includes('_笨笨笨蛋Luzi2'));
+        // 输出结果
+        AssetLoad(filteredAssetFemale3DCG, "Female3DCG", AssetFemale3DCGExtended);
+    }
+
     function addExtraExpressionsToAssets() {
         var Emoticon内容 = ["车车_Luzi", "衣架_Luzi", "电话_Luzi", "灯泡_Luzi", "警告_Luzi", "心_Luzi", "画画_Luzi", "符号_Luzi", "视频_Luzi",];
         var GroupEmoticon = AssetFemale3DCG.filter(A => A.Group === "Emoticon");
@@ -2362,6 +2452,9 @@
             AssetGroupAdd("Female3DCG", addAssetGroup.Liquid2_Luzi[0])
             AssetGroupAdd("Female3DCG", addAssetGroup.BodyMarkings2_Luzi[0])
 
+            addExtraOutfitsToAssets();
+            addExtraOutfitsToAssets2();
+
             addExtraExpressionsToAssets();
             mergeAddAssetIntoFemale3DCGAssets();
 
@@ -2382,10 +2475,38 @@
 
             if (AssetGroup) {        // 用于修改服装组名
                 const descriptionMap = new Map([
+                    ['Cloth_笨笨蛋Luzi', '🍔衣服2'],
+                    ['ClothLower_笨笨蛋Luzi', '🍔下装2'],
+                    ['Cloth_笨笨笨蛋Luzi2', '🍔衣服3'],
+                    ['ClothLower_笨笨笨蛋Luzi2', '🍔下装3'],
+                    ['Panties_笨笨蛋Luzi', '🍔内裤2'],
+                    ['ClothAccessory_笨笨蛋Luzi', '🍔服装配饰2'],
+                    ['Necklace_笨笨蛋Luzi', '🍔项链2'],
+                    ['Bra_笨笨蛋Luzi', '🍔胸罩2'],
+                    ['Shoes_笨笨蛋Luzi', '🍔鞋子2'],
+                    ['Hat_笨笨蛋Luzi', '🍔帽子2'],
+                    ['HairAccessory3_笨笨蛋Luzi', '🍔发饰2'],
+                    ['Gloves_笨笨蛋Luzi', '🍔手套2'],
+                    ['Mask_笨笨蛋Luzi', '🍔面具2'],
+                    ['Wings_笨笨蛋Luzi', '🍔翅膀2'],
                     ['MISSING ASSETGROUP DESCRIPTION: Liquid2_Luzi', '🍔液体2_Luzi'],
                     ['MISSING ASSETGROUP DESCRIPTION: BodyMarkings2_Luzi', '🍔身体涂画2'],
                 ]);
                 const descriptionMap2 = new Map([
+                    ['Cloth_笨笨蛋Luzi', '🍔Cloth2'],
+                    ['ClothLower_笨笨蛋Luzi', '🍔Bottom2'],
+                    ['Cloth_笨笨笨蛋Luzi2', '🍔Cloth3'],
+                    ['ClothLower_笨笨笨蛋Luzi2', '🍔Bottom3'],
+                    ['Panties_笨笨蛋Luzi', '🍔Panties2'],
+                    ['ClothAccessory_笨笨蛋Luzi', '🍔Cloth Accessory2'],
+                    ['Necklace_笨笨蛋Luzi', '🍔Necklace2'],
+                    ['Bra_笨笨蛋Luzi', '🍔Bra2'],
+                    ['Shoes_笨笨蛋Luzi', '🍔Shoes2'],
+                    ['Hat_笨笨蛋Luzi', '🍔Hat2'],
+                    ['HairAccessory3_笨笨蛋Luzi', '🍔Hair Accessory2'],
+                    ['Gloves_笨笨蛋Luzi', '🍔Gloves2'],
+                    ['Mask_笨笨蛋Luzi', '🍔Mask2'],
+                    ['Wings_笨笨蛋Luzi', '🍔Wings2'],
                     ['MISSING ASSETGROUP DESCRIPTION: Liquid2_Luzi', '🍔Liquid2_Luzi'],
                     ['MISSING ASSETGROUP DESCRIPTION: BodyMarkings2_Luzi', '🍔BodyMarkings2'],
                 ]);
@@ -2497,6 +2618,66 @@
                 }
             }
 
+
+            // 用于替换🍔组内的服装名字 ============================
+            const nameMap = {
+                'Cloth': 'Cloth_笨笨蛋Luzi',
+                'ClothLower': 'ClothLower_笨笨蛋Luzi',
+                'Panties': 'Panties_笨笨蛋Luzi',
+                'ClothAccessory': 'ClothAccessory_笨笨蛋Luzi',
+                'Necklace': 'Necklace_笨笨蛋Luzi',
+                'Bra': 'Bra_笨笨蛋Luzi',
+                'Shoes': 'Shoes_笨笨蛋Luzi',
+                'Hat': 'Hat_笨笨蛋Luzi',
+                'HairAccessory3': 'HairAccessory3_笨笨蛋Luzi',
+                'Gloves': 'Gloves_笨笨蛋Luzi',
+                'Mask': 'Mask_笨笨蛋Luzi',
+                'Wings': 'Wings_笨笨蛋Luzi'
+            };
+
+            function replaceDescription(baseName, luZiName) {
+                const baseAssets = AssetGroup.find(item => item.Name === baseName)?.Asset;
+                const luZiAssets = AssetGroup.find(item => item.Name === luZiName)?.Asset;
+
+                if (baseAssets && luZiAssets) {
+                    baseAssets.forEach(baseAsset => {
+                        const matchingAsset = luZiAssets.find(asset => asset.Name === baseAsset.Name);
+                        if (matchingAsset) {
+                            matchingAsset.Description = baseAsset.Description;
+                        }
+                    });
+                }
+            }
+
+            for (const baseName in nameMap) {
+                replaceDescription(baseName, nameMap[baseName]);
+            }
+
+
+            const nameMap2 = {
+                'Cloth': 'Cloth_笨笨笨蛋Luzi2',
+                'ClothLower': 'ClothLower_笨笨笨蛋Luzi2',
+            };
+
+            function replaceDescription2(baseName, luZiName) {
+                const baseAssets = AssetGroup.find(item => item.Name === baseName)?.Asset;
+                const luZiAssets = AssetGroup.find(item => item.Name === luZiName)?.Asset;
+
+                if (baseAssets && luZiAssets) {
+                    baseAssets.forEach(baseAsset => {
+                        const matchingAsset = luZiAssets.find(asset => asset.Name === baseAsset.Name);
+                        if (matchingAsset) {
+                            matchingAsset.Description = baseAsset.Description;
+                        }
+                    });
+                }
+            }
+
+            for (const baseName in nameMap2) {
+                replaceDescription2(baseName, nameMap2[baseName]);
+            }
+
+            // ========================================================
 
             let addAssetStringSCVCN = {
                 ItemTorso触手服_LuziSelectBase: '选择配置',
