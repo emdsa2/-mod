ffd261daf2a50859d1ba5ddac4c19b038e9f2e69
emdsa2 <1260556316@qq.com>
2024-07-30 11:51:36 +0800

    添加 蜘蛛

--- modbeta.js
+++ modbeta.js
@@ -1754,6 +1754,42 @@
                     },
                 ],
             },
+            {
+                Name: "蜘蛛_Luzi", Random: false, Gender: "F",
+                Top: 0, Left: -156,
+                Difficulty: 25,
+                Hide: ["Pussy", "BodyLower", "BodyFull"],
+                // Extended: true,
+                Layer: [
+                    {
+                        Name: "肚子衔接", Priority: 16,
+                        ParentGroup: "BodyLower",
+                    },
+                    {
+                        Name: "肚子", Priority: 6,
+                        ParentGroup: "BodyLower",
+                    },
+                    {
+                        Name: "爪子前", Priority: 26,
+                        ParentGroup: "BodyLower",
+                    },
+                    {
+                        Name: "爪子中", Priority: 5,
+                        ParentGroup: "BodyLower",
+                    },
+                    {
+                        Name: "爪子后", Priority: 4,
+                        ParentGroup: "BodyLower",
+                    },
+
+
+                ],
+                OverrideHeight: {
+                    Height: -230,
+                    Priority: 21,
+                    HeightRatioProportion: 0,
+                },
+            },
         ],
         ItemVulva: [
             {
