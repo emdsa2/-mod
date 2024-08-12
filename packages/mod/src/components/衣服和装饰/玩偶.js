8d8f36aa22e6950cf30b2043b1438a0017282b75
emdsa2 <1260556316@qq.com>
2024-08-12 20:31:49 +0800

    添加玩偶

--- packages/mod/src/components/衣服和装饰/玩偶.js
+++ packages/mod/src/components/衣服和装饰/玩偶.js
@@ -19,64 +19,227 @@ const asset = {
         AllFours: "Hide",
     },
     Layer: [
+        // 玩具店
         {
-            Name: "Ada",
+            Name: "Saki",
             Top: 0,
             Left: 0,
-            AllowTypes: { typed: [1] }
+            AllowTypes: { d: 1 },
         },
         {
             Name: "Luzi",
             Top: 0,
             Left: 0,
-            AllowTypes: { typed: [2] }
+            AllowTypes: { d: 2 },
         },
+
+        // 狼窝
         {
-            Name: "Saki",
+            Name: "吉娜",
             Top: 0,
             Left: 0,
-            AllowTypes: { typed: [3] }
+            AllowTypes: { s: 1 },
+        },
+        {
+            Name: "Ada",
+            Top: 0,
+            Left: 0,
+            AllowTypes: { s: 2 },
+        },
+        {
+            Name: "Luzi2",
+            Top: 0,
+            Left: 0,
+            AllowTypes: { s: 3 },
+        },
+
+        // 芷窝
+        {
+            Name: "Ttong",
+            Top: 0,
+            Left: 0,
+            AllowTypes: { z: 1 },
+        },
+        {
+            Name: "ZforShort",
+            Top: 0,
+            Left: 0,
+            AllowTypes: { z: 2 },
+        },
+        {
+            Name: "Gin",
+            Top: 0,
+            Left: 0,
+            AllowTypes: { z: 3 },
+        },
+        {
+            Name: "Echo",
+            Top: 0,
+            Left: 0,
+            AllowTypes: { z: 4 },
+        },
+
+        // Catnest
+        {
+            Name: "XinLian",
+            Top: 0,
+            Left: 0,
+            AllowTypes: { c: 1 },
+        },
+        {
+            Name: "Cyäegha",
+            Top: 0,
+            Left: 0,
+            AllowTypes: { c: 2 },
+        },
+        {
+            Name: "PumpkinPie",
+            Top: 0,
+            Left: 0,
+            AllowTypes: { c: 3 },
         },
     ],
 };
 
+
+/** @type {ModularItemConfig} */
 const extended = {
-    Archetype: ExtendedArchetype.TYPED,
-    DrawImages: false,
-    Options: [{ Name: "无" }, { Name: "Ada" }, { Name: "Luzi" }, { Name: "Saki" }],
+    Archetype: ExtendedArchetype.MODULAR,
+    ChangeWhenLocked: false,
+    Modules: [
+        {
+            Name: "玩具店玩偶",
+            DrawImages: false,
+            Key: "d",
+            Options: [{}, {}, {}],
+        },
+        {
+            Name: "狼窝玩偶",
+            DrawImages: false,
+            Key: "s",
+            Options: [{}, {}, {}, {}],
+        },
+        {
+            Name: "芷窝玩偶",
+            DrawImages: false,
+            Key: "z",
+            Options: [{}, {}, {}, {}, {}],
+        },
+        {
+            Name: "Catnest玩偶",
+            DrawImages: false,
+            Key: "c",
+            Options: [{}, {}, {}, {}],
+        },
+    ],
 };
 
 /** @type {Translation.Dialog} */
-const dialog = {
+const dialogs = {
     CN: {
-        ItemMisc玩偶_LuziSelect: "选择玩偶",
-        ItemMisc玩偶_Luzi无: "无",
-        ItemMisc玩偶_LuziAda: "Ada",
-        ItemMisc玩偶_LuziLuzi: "Luzi",
-        ItemMisc玩偶_LuziSaki: "Saki",
-        
-        ItemMisc玩偶_LuziSet无: "SourceCharacter移除了DestinationCharacter手里的玩偶.",
-        ItemMisc玩偶_LuziSetAda: "SourceCharacter给了DestinationCharacter一只Ada玩偶.",
-        ItemMisc玩偶_LuziSetLuzi: "SourceCharacter给了DestinationCharacter一只Luzi玩偶.",
-        ItemMisc玩偶_LuziSetSaki: "SourceCharacter给了DestinationCharacter一只Saki玩偶.",
+        ItemMisc玩偶_LuziSelectBase: "选择房间",
+        ItemMisc玩偶_LuziSelect玩具店玩偶: "选择玩具店玩偶",
+        ItemMisc玩偶_LuziModule玩具店玩偶: "玩具店玩偶",
+        ItemMisc玩偶_LuziOptiond0: "空",
+        ItemMisc玩偶_LuziOptiond1: "Saki",
+        ItemMisc玩偶_LuziOptiond2: "Luzi",
+        ItemMisc玩偶_LuziSetd0: "SourceCharacter移除了DestinationCharacter手上的玩偶.",
+        ItemMisc玩偶_LuziSetd1: "SourceCharacter给了DestinationCharacter一只Saki玩偶.",
+        ItemMisc玩偶_LuziSetd2: "SourceCharacter给了DestinationCharacter一只Luzi玩偶.",
+
+
+        ItemMisc玩偶_LuziSelect狼窝玩偶: "选择狼窝玩偶",
+        ItemMisc玩偶_LuziModule狼窝玩偶: "狼窝玩偶",
+        ItemMisc玩偶_LuziOptions0: "空",
+        ItemMisc玩偶_LuziOptions1: "吉娜",
+        ItemMisc玩偶_LuziOptions2: "Ada",
+        ItemMisc玩偶_LuziOptions3: "Luzi",
+        ItemMisc玩偶_LuziSets0: "SourceCharacter移除了DestinationCharacter手上的玩偶.",
+        ItemMisc玩偶_LuziSets1: "SourceCharacter给了DestinationCharacter一只吉娜玩偶.",
+        ItemMisc玩偶_LuziSets2: "SourceCharacter给了DestinationCharacter一只Ada玩偶.",
+        ItemMisc玩偶_LuziSets3: "SourceCharacter给了DestinationCharacter一只Luzi玩偶.",
+
+
+        ItemMisc玩偶_LuziSelect芷窝玩偶: "选择芷窝玩偶",
+        ItemMisc玩偶_LuziModule芷窝玩偶: "芷窝玩偶",
+        ItemMisc玩偶_LuziOptionz0: "空",
+        ItemMisc玩偶_LuziOptionz1: "Ttong",
+        ItemMisc玩偶_LuziOptionz2: "ZforShort",
+        ItemMisc玩偶_LuziOptionz3: "Gin",
+        ItemMisc玩偶_LuziOptionz4: "Echo",
+        ItemMisc玩偶_LuziSetz0: "SourceCharacter移除了DestinationCharacter手上的玩偶.",
+        ItemMisc玩偶_LuziSetz1: "SourceCharacter给了DestinationCharacter一只Ttong玩偶.",
+        ItemMisc玩偶_LuziSetz2: "SourceCharacter给了DestinationCharacter一只ZforShort玩偶.",
+        ItemMisc玩偶_LuziSetz3: "SourceCharacter给了DestinationCharacter一只Gin玩偶.",
+        ItemMisc玩偶_LuziSetz4: "SourceCharacter给了DestinationCharacter一只Echo玩偶.",
+
+        ItemMisc玩偶_LuziSelectCatnest玩偶: "选择Catnest玩偶",
+        ItemMisc玩偶_LuziModuleCatnest玩偶: "Catnest玩偶",
+        ItemMisc玩偶_LuziOptionc0: "空",
+        ItemMisc玩偶_LuziOptionc1: "XinLian",
+        ItemMisc玩偶_LuziOptionc2: "Cyäegha",
+        ItemMisc玩偶_LuziOptionc3: "PumpkinPie",
+        ItemMisc玩偶_LuziSetc0: "SourceCharacter移除了DestinationCharacter手上的玩偶.",
+        ItemMisc玩偶_LuziSetc1: "SourceCharacter给了DestinationCharacter一只XinLian玩偶.",
+        ItemMisc玩偶_LuziSetc2: "SourceCharacter给了DestinationCharacter一只Cyäegha玩偶.",
+        ItemMisc玩偶_LuziSetc3: "SourceCharacter给了DestinationCharacter一只PumpkinPie玩偶.",
+
+
     },
     EN: {
-        ItemMisc玩偶_LuziSelect: "Select doll",
-        ItemMisc玩偶_Luzi无: "None",
-        ItemMisc玩偶_LuziAda: "Ada",
-        ItemMisc玩偶_LuziLuzi: "Luzi",
-        ItemMisc玩偶_LuziSaki: "Saki",
+        ItemMisc玩偶_LuziSelectBase: "选择房间",
+        ItemMisc玩偶_LuziSelect玩具店玩偶: "选择玩具店玩偶",
+        ItemMisc玩偶_LuziModule玩具店玩偶: "玩具店玩偶",
+        ItemMisc玩偶_LuziOptiond0: "空",
+        ItemMisc玩偶_LuziOptiond1: "Saki",
+        ItemMisc玩偶_LuziOptiond2: "Luzi",
+        ItemMisc玩偶_LuziSetd0: "SourceCharacter移除了DestinationCharacter手上的玩偶.",
+        ItemMisc玩偶_LuziSetd1: "SourceCharacter给了DestinationCharacter一只Saki玩偶.",
+        ItemMisc玩偶_LuziSetd2: "SourceCharacter给了DestinationCharacter一只Luzi玩偶.",
 
-        ItemMisc玩偶_LuziSet无: "SourceCharacter移除了DestinationCharacter手里的玩偶.",
-        ItemMisc玩偶_LuziSetAda: "SourceCharacter给了DestinationCharacter一只Ada玩偶.",
-        ItemMisc玩偶_LuziSetLuzi: "SourceCharacter给了DestinationCharacter一只Luzi玩偶.",
-        ItemMisc玩偶_LuziSetSaki: "SourceCharacter给了DestinationCharacter一只Saki玩偶.",
+
+        ItemMisc玩偶_LuziSelect狼窝玩偶: "选择狼窝玩偶",
+        ItemMisc玩偶_LuziModule狼窝玩偶: "狼窝玩偶",
+        ItemMisc玩偶_LuziOptions0: "空",
+        ItemMisc玩偶_LuziOptions1: "吉娜",
+        ItemMisc玩偶_LuziOptions2: "Ada",
+        ItemMisc玩偶_LuziOptions3: "Luzi",
+        ItemMisc玩偶_LuziSets0: "SourceCharacter移除了DestinationCharacter手上的玩偶.",
+        ItemMisc玩偶_LuziSets1: "SourceCharacter给了DestinationCharacter一只吉娜玩偶.",
+        ItemMisc玩偶_LuziSets2: "SourceCharacter给了DestinationCharacter一只Ada玩偶.",
+        ItemMisc玩偶_LuziSets3: "SourceCharacter给了DestinationCharacter一只Luzi玩偶.",
+
+
+        ItemMisc玩偶_LuziSelect芷窝玩偶: "选择芷窝玩偶",
+        ItemMisc玩偶_LuziModule芷窝玩偶: "芷窝玩偶",
+        ItemMisc玩偶_LuziOptionz0: "空",
+        ItemMisc玩偶_LuziOptionz1: "Ttong",
+        ItemMisc玩偶_LuziOptionz2: "ZforShort",
+        ItemMisc玩偶_LuziOptionz3: "Gin",
+        ItemMisc玩偶_LuziOptionz4: "Echo",
+        ItemMisc玩偶_LuziSetz0: "SourceCharacter移除了DestinationCharacter手上的玩偶.",
+        ItemMisc玩偶_LuziSetz1: "SourceCharacter给了DestinationCharacter一只Ttong玩偶.",
+        ItemMisc玩偶_LuziSetz2: "SourceCharacter给了DestinationCharacter一只ZforShort玩偶.",
+        ItemMisc玩偶_LuziSetz3: "SourceCharacter给了DestinationCharacter一只Gin玩偶.",
+        ItemMisc玩偶_LuziSetz4: "SourceCharacter给了DestinationCharacter一只Echo玩偶.",
+
+        ItemMisc玩偶_LuziSelectCatnest玩偶: "选择Catnest玩偶",
+        ItemMisc玩偶_LuziModuleCatnest玩偶: "Catnest玩偶",
+        ItemMisc玩偶_LuziOptionc0: "空",
+        ItemMisc玩偶_LuziOptionc1: "XinLian",
+        ItemMisc玩偶_LuziOptionc2: "Cyäegha",
+        ItemMisc玩偶_LuziOptionc3: "PumpkinPie",
+        ItemMisc玩偶_LuziSetc0: "SourceCharacter移除了DestinationCharacter手上的玩偶.",
+        ItemMisc玩偶_LuziSetc1: "SourceCharacter给了DestinationCharacter一只XinLian玩偶.",
+        ItemMisc玩偶_LuziSetc2: "SourceCharacter给了DestinationCharacter一只Cyäegha玩偶.",
+        ItemMisc玩偶_LuziSetc3: "SourceCharacter给了DestinationCharacter一只PumpkinPie玩偶.",
+
     },
 };
 
-const translations = { CN: "玩偶", EN: "doll" };
+const translations = { CN: "玩偶", EN: "Doll" };
 
 export default function () {
     AssetManager.addAsset("ItemMisc", asset, extended, translations);
-    AssetManager.addCustomDialog(dialog);
+    AssetManager.addCustomDialog(dialogs);
 }
