195cd9b4c6e36e7bf7fc4107854f4e1a0a62f56a
emdsa2 <1260556316@qq.com>
2024-08-31 06:39:47 +0800

    摇晃耳朵

---
+++ packages/mod/src/components/衣服和装饰/耳朵_镜像.js
@@ -0,0 +1,47 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomAssetDefinitionAppearance[]} */
+const assets = [
+    {
+        Name: "黑猫耳镜像_Luzi",
+        Random: false,
+        Fetish: ["Pet"],
+        BodyCosplay: true,
+        Layer: [
+            { Name: "Outer", HideForAttribute: ["ShortHair"] },
+            { Name: "Inner", HideForAttribute: ["ShortHair"] },
+            {
+                Name: "OuterShort",
+                ShowForAttribute: ["ShortHair"],
+                CopyLayerColor: "Outer",
+                Top: 30,
+                Left: 115,
+            },
+            {
+                Name: "InnerShort",
+                ShowForAttribute: ["ShortHair"],
+                CopyLayerColor: "Inner",
+                Top: 30,
+                Left: 115,
+            },
+        ],
+    },
+];
+
+/** @type { Translation.GroupedEntries } */
+const translations = {
+    CN: {
+        HairAccessory2: {
+            黑猫耳镜像_Luzi: "黑猫耳镜像",
+        },
+    },
+    EN: {
+        HairAccessory2: {
+            黑猫耳镜像_Luzi: "BlackCatEarsMirror",
+        },
+    },
+};
+
+export default function () {
+    AssetManager.addGroupedAssets({ HairAccessory2: assets }, translations);
+}
--- packages/mod2/src/components/摇晃.js
+++ packages/mod2/src/components/摇晃.js
@@ -6,20 +6,20 @@ import ChatRoomOrder from "@mod-utils/ChatRoomOrder";
 * @param {string} item1 - 道具名称 1
 * @param {string} item2 - 道具名称 2
 */// @ts-ignore
-function 摇晃尾巴(player, item1, item2) {
+function shakeTail(player, itemgroup, item1, item2) {
     for (let i = 0; i < 4; i++) {
         setTimeout(() => {
-            if (InventoryGet(Player, "TailStraps").Asset.Name === item1) {
-                let 道具颜色 = InventoryGet(player, "TailStraps").Color
+            if (InventoryGet(player, itemgroup).Asset.Name === item1) {
+                let propColor = InventoryGet(player, itemgroup).Color;
 
-                InventoryWear(player, item2, "TailStraps", 道具颜色);
-                ChatRoomCharacterUpdate(player); // 更新外观
+                InventoryWear(player, item2, itemgroup, propColor);
+                ChatRoomCharacterUpdate(player); // Update appearance
 
-            } else if (InventoryGet(player, "TailStraps").Asset.Name === item2) {
-                let 道具颜色 = InventoryGet(player, "TailStraps").Color
+            } else if (InventoryGet(player, itemgroup).Asset.Name === item2) {
+                let propColor = InventoryGet(player, itemgroup).Color;
 
-                InventoryWear(player, item1, "TailStraps", 道具颜色);
-                ChatRoomCharacterUpdate(player); // 更新外观
+                InventoryWear(player, item1, itemgroup, propColor);
+                ChatRoomCharacterUpdate(player); // Update appearance
             }
         }, 200 * i);
     }
@@ -39,16 +39,16 @@ const activities = [
             if (info.SourceCharacter === player.MemberNumber) {
                 const asset = AssetGet("Female3DCG", "TailStraps", "穿戴式猫尾镜像_Luzi");
                 if (!asset) return;
-                摇晃尾巴(player, "TailStrap", "穿戴式猫尾镜像_Luzi");
-                摇晃尾巴(player, "PuppyTailStrap", "穿戴式狗尾镜像_Luzi");
-                摇晃尾巴(player, "PuppyTailStrap1", "穿戴式软小狗尾镜像_Luzi");
-                摇晃尾巴(player, "KittenTailStrap1", "穿戴式浅色猫尾镜像_Luzi");
-                摇晃尾巴(player, "KittenTailStrap2", "小型穿戴式软猫尾镜像_Luzi");
-                摇晃尾巴(player, "FoxTailStrap1", "FoxTailStrap2");
-                摇晃尾巴(player, "WolfTailStrap1", "大型穿戴式狼尾镜像_Luzi");
-                摇晃尾巴(player, "WolfTailStrap2", "小型穿戴式狼尾镜像_Luzi");
-                摇晃尾巴(player, "WolfTailStrap3", "白色穿戴式狼尾镜像_Luzi");
-                摇晃尾巴(player, "DragonTailStrap2Left", "DragonTailStrap2Right");
+                shakeTail(player, "TailStraps", "TailStrap", "穿戴式猫尾镜像_Luzi");
+                shakeTail(player, "TailStraps", "PuppyTailStrap", "穿戴式狗尾镜像_Luzi");
+                shakeTail(player, "TailStraps", "PuppyTailStrap1", "穿戴式软小狗尾镜像_Luzi");
+                shakeTail(player, "TailStraps", "KittenTailStrap1", "穿戴式浅色猫尾镜像_Luzi");
+                shakeTail(player, "TailStraps", "KittenTailStrap2", "小型穿戴式软猫尾镜像_Luzi");
+                shakeTail(player, "TailStraps", "FoxTailStrap1", "FoxTailStrap2");
+                shakeTail(player, "TailStraps", "WolfTailStrap1", "大型穿戴式狼尾镜像_Luzi");
+                shakeTail(player, "TailStraps", "WolfTailStrap2", "小型穿戴式狼尾镜像_Luzi");
+                shakeTail(player, "TailStraps", "WolfTailStrap3", "白色穿戴式狼尾镜像_Luzi");
+                shakeTail(player, "TailStraps", "DragonTailStrap2Left", "DragonTailStrap2Right");
             }
         },
         useImage: "Wiggle",
@@ -59,6 +59,33 @@ const activities = [
         dialog: {
             CN: "SourceCharacter摇晃PronounPossessive的尾巴.",
             EN: "SourceCharacter wags PronounPossessive tail.",
+        },
+    },
+    {
+        activity: {
+            Name: "摇晃耳朵",
+            Prerequisite: [
+                (prereq, acting, acted, group) => !!InventoryGet(acted, "HairAccessory2"),
+            ],
+            MaxProgress: 50,
+            Target: [],
+            TargetSelf: ["ItemEars"],
+        },
+        run: (player, sender, info) => {
+            if (info.SourceCharacter === player.MemberNumber) {
+                const asset = AssetGet("Female3DCG", "HairAccessory2", "黑猫耳镜像_Luzi");
+                if (!asset) return;
+                shakeTail(player, "HairAccessory2", "KittenEars1", "黑猫耳镜像_Luzi");
+            }
+        },
+        useImage: "Wiggle",
+        label: {
+            CN: "摇晃耳朵",
+            EN: "Wag Ears",
+        },
+        dialog: {
+            CN: "SourceCharacter摇晃PronounPossessive的耳朵.",
+            EN: "SourceCharacter wags PronounPossessive's ears.",
         },
     },
 ];
