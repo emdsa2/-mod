ef98dd2ecaa178dbb007b1308dedbe3d810a8f86
emdsa2 <1260556316@qq.com>
2024-08-30 20:50:40 +0800

    新帽子 西装替换小号 新妆容

---
+++ packages/mod/src/components/衣服和装饰/帽子.js
@@ -0,0 +1,53 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+
+/** @type { CustomGroupedAssetDefinitions }} */
+const assets = {
+    Hat: [
+        {
+            Name: "冕旒_Luzi",
+            Random: false,
+            Top: -18,
+            Left: 0,
+            DefaultColor: ["##660606","#000000","#C18A34","#CFAC68"],
+            Layer: [
+                {
+                    Name: "帽顶",
+                    Priority: 55,
+                
+                },
+                {
+                    Name: "帽身",
+                    Priority: 55,
+                },
+                {
+                    Name: "纹样",
+                    Priority: 55,
+                },
+                {
+                    Name: "帽帘",
+                    Priority: 55,
+                },
+            ],
+        },
+    ],
+};
+
+/** @type { Translation.GroupedEntries } */
+const translations = {
+    CN: {
+        Hat: {
+            冕旒_Luzi: "冕旒",
+        },
+    },
+    EN: {
+        Hat: {
+            冕旒_Luzi: "冕旒",
+        },
+    },
+};
+
+export default function () {
+    AssetManager.addGroupedAssets(assets, translations);
+}
+
---
+++ packages/mod/src/components/衣服和装饰/面部妆容.js
@@ -0,0 +1,34 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomGroupedAssetDefinitions} */
+const assets = {
+    EyeShadow: [
+        {
+            Name: "面部妆容_Luzi",
+            Random: false,
+            Top: 0,
+            Left: 0,
+            DefaultColor: ["#000000"],
+        },
+    ],
+};
+
+/** @type { Translation.GroupedEntries } */
+const translations = {
+    CN: {
+        EyeShadow: {
+            面部妆容_Luzi: "面部妆容",
+        },
+
+    },
+    EN: {
+        EyeShadow: {
+            面部妆容_Luzi: "面部妆容",
+        },
+
+    },
+};
+
+export default function () {
+    AssetManager.addGroupedAssets(assets, translations);
+}
