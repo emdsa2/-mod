df13daf6f6249bf3d16ea40165f604b8d21f608a
XinLian132243 <XinLian132243@protonmail.com>
2024-10-26 18:31:12 +0800

    瑜伽裤和半开百褶裙的配置

---
+++ packages/mod/src/components/衣服和装饰/半开百褶裙.js
@@ -0,0 +1,51 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "半开百褶裙_Luzi",
+    Random: false,
+    Top: 0,
+    Left: 0,
+    DefaultColor: ["#7F1739", "Default", "Default"],
+    PoseMapping: {
+        Kneel: "KneelingSpread",
+        Hogtied: PoseType.HIDE,
+        AllFours: PoseType.HIDE,
+    },
+    Layer: [    
+        {
+            Name: "裙子",
+            Priority: 26         
+        }, 
+        {
+            Name: "白线",
+            Priority: 26        
+        },    
+        {
+            Name: "链子",
+            Priority: 26         
+        }
+    ],
+};
+
+/** @type {Translation.Entry} */
+const translation = {
+    CN: "半开百褶裙",
+    EN: "Half pleated skirt",
+};
+
+export default function () {
+  
+    AssetManager.addAsset(
+        "ClothLower",
+        {
+            ...asset,
+            Left: {
+                [PoseType.DEFAULT]: 0,
+                KneelingSpread: 90,
+            },
+        },
+        undefined,
+        translation
+    );
+}
---
+++ packages/mod/src/components/衣服和装饰/瑜伽裤.js
@@ -0,0 +1,57 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "瑜伽裤_Luzi",
+    Random: false,
+    Top: 0,
+    Left: 0,
+    Layer: [
+        {
+            Name: "上",
+            Priority: 24, 
+            PoseMapping: {
+                LegsClosed: PoseType.DEFAULT,
+                KneelingSpread: PoseType.DEFAULT,
+                Kneel: PoseType.DEFAULT,
+                Hogtied: PoseType.HIDE,
+                AllFours: PoseType.HIDE,
+            },
+        },
+        {
+            Name: "下",
+            Priority: 24,
+            PoseMapping: {
+                LegsClosed: "LegsClosed",
+                KneelingSpread: "KneelingSpread",
+                Kneel: "Kneel",
+                Hogtied: PoseType.HIDE,
+                AllFours: PoseType.HIDE,
+            },
+        }
+    ],
+};
+
+/** @type {Translation.Entry} */
+const translation = {
+    CN: "瑜伽裤",
+    EN: "Yoga Pants",
+};
+
+export default function () {
+
+    AssetManager.addAsset(
+        "ClothLower",
+        {
+            ...asset,
+            Left: {
+                [PoseType.DEFAULT]: 0,
+                KneelingSpread: 90,
+            },
+        },
+        undefined,
+        translation
+    );
+
+    AssetManager.addAsset("SuitLower", asset, null, translation);
+}
