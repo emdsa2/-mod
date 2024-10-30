467b75610d22fd019020b117cf93c0c2d6f68d10
XinLian132243 <XinLian132243@protonmail.com>
2024-10-31 04:44:00 +0800

    南瓜马具口塞

---
+++ packages/mod/src/components/束缚与玩具/南瓜马具口塞.js
@@ -0,0 +1,56 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "南瓜马具口塞",
+    Random: false,
+    Top: 0,
+    Left: 0,
+    Priority: 36,
+    ParentGroup: null,
+    Fetish: ["Leather"],
+    Value: 60,
+    Difficulty: 6,
+    Time: 20,
+    AllowLock: true,
+    AllowTighten: true,
+    Prerequisite: "GagUnique",
+    Hide: ["Mouth"],
+    Effect: [E.BlockMouth, E.GagMedium],
+    ExpressionTrigger: [{ Name: "DroolSides", Group: "Fluids", Timer: 30 }],
+    Layer: [
+        {
+            Name: "带子",
+        },
+        {
+            Name: "金属",
+        },
+        {
+            Name: "南瓜",
+        },
+        {
+            Name: "眼睛",
+        }
+    ],
+    PoseMapping: {
+        TapedHands: PoseType.DEFAULT,
+        Yoked: PoseType.DEFAULT,
+        OverTheHead: PoseType.DEFAULT,
+        BackBoxTie: PoseType.DEFAULT,
+        BackElbowTouch: PoseType.DEFAULT,
+        BackCuffs: PoseType.DEFAULT,
+        Hogtied: PoseType.DEFAULT,
+        AllFours: PoseType.DEFAULT,
+    },
+};
+
+const translations = {
+    CN: "南瓜马具口塞",
+    EN: "Pumpkin Harness Gag",
+    RU: "кляп из тыквы",
+    UA: "гарбуз джгут кляп",
+};
+
+export default function () {
+    AssetManager.addAsset("ItemMouth", asset, undefined, translations);
+}
