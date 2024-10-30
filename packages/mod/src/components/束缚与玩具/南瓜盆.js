058665b7634cc465c2cab96e65878640bf09bc1f
XinLian132243 <XinLian132243@protonmail.com>
2024-10-31 04:19:13 +0800

    南瓜盆图片和配置

---
+++ packages/mod/src/components/束缚与玩具/南瓜盆.js
@@ -0,0 +1,68 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "南瓜盆",
+    Random: false,
+    Top: 0,
+    Left: 0,
+    Gender: "F",
+    Fetish: ["Sadism"],
+    Priority: 48,
+    ParentGroup: null,
+    Layer: [
+        {
+            Name: "南瓜",
+        },
+        {
+            Name: "糖果",
+        },
+        {
+            Name: "眼睛",
+        },
+        {
+            Name: "链子",
+            AllowTypes: { typed: 1 },
+        }
+    ],
+};
+
+
+/** @type {AssetArchetypeConfig} */
+const extended = {
+    Archetype: ExtendedArchetype.TYPED,
+    DrawImages: false,
+    Options: [
+        { Name: "无" },
+        { Name: "链子" },
+    ],
+};
+
+/** @type {Translation.Dialog} */
+const dialog = {
+    CN: {
+        ItemMisc南瓜盆Select: "设置",
+        ItemMisc南瓜盆无: "无",
+        ItemMisc南瓜盆链子: "链子",
+        
+        ItemMisc南瓜盆Set无: "SourceCharacter取下了DestinationCharacter的南瓜盆上的链子.",
+        ItemMisc南瓜盆Set链子: "SourceCharacter给DestinationCharacter的南瓜盆连上了一条链子.",
+    },
+    EN: {
+        ItemMisc南瓜盆Select: "Setting",
+        ItemMisc南瓜盆无: "None",
+        ItemMisc南瓜盆链子: "Chain",
+        
+        ItemMisc南瓜盆Set无: "SourceCharacter removes the chain from DestinationCharacter's pumpkin pot.",
+        ItemMisc南瓜盆Set链子: "SourceCharacter connects a link to DestinationCharacter's pumpkin pot.",
+    }
+};
+
+const translations = { 
+     CN: "南瓜盆",
+     EN: "Pumpkin Pot" };
+
+export default function () {
+    AssetManager.addAsset("ItemMisc", asset, extended, translations);
+    AssetManager.addCustomDialog(dialog);
+}
