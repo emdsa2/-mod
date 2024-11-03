2030bf9cd58021fd9c0130f686d0bf605b9d4799
emdsa2 <1260556316@qq.com>
2024-11-04 04:31:36 +0800

    添加披肩

---
+++ packages/mod/src/components/衣服和装饰/披肩.js
@@ -0,0 +1,52 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "披肩",
+    Random: false,
+    Gender: "F",
+    Top: -110,
+    Left: 0,
+    Prerequisite: ["HasBreasts"],
+    Layer: [
+        {
+            Name: "Band",
+            Priority: 26,
+            PoseMapping: {
+                TapedHands: PoseType.DEFAULT,
+                Yoked: "Yoked",
+                OverTheHead: "OverTheHead",
+                BackBoxTie: PoseType.DEFAULT,
+                BackElbowTouch: PoseType.DEFAULT,
+                BackCuffs: PoseType.DEFAULT,
+                AllFours: PoseType.DEFAULT,
+                Hogtied: PoseType.DEFAULT,
+            },
+        },
+        {
+            Name: "Shawl",
+            Priority: 26,
+            PoseMapping: {
+                TapedHands: PoseType.DEFAULT,
+                Yoked: PoseType.DEFAULT,
+                OverTheHead: PoseType.DEFAULT,
+                BackBoxTie: PoseType.DEFAULT,
+                BackElbowTouch: PoseType.DEFAULT,
+                BackCuffs: PoseType.DEFAULT,
+                AllFours: PoseType.DEFAULT,
+                Hogtied: PoseType.DEFAULT,
+            },
+        },
+    ],
+};
+
+const translations = {
+    CN: "披肩",
+    EN: "Shawl",
+    RU: "Палантин",
+    UA: "Палантин",
+};
+
+export default function () {
+    AssetManager.addAsset("Cloth", asset, undefined, translations);
+}
