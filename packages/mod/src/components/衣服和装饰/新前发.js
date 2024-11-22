d179a71f7b095b6cb107954b58bd254c0e080248
XinLian132243 <XinLian132243@protonmail.com>
2024-11-23 01:55:38 +0800

    3个前发

--- packages/mod/src/components/衣服和装饰/新前发.js
+++ packages/mod/src/components/衣服和装饰/新前发.js
@@ -145,6 +145,49 @@ const assets = {
             ],
         },
         {
+            Name: "前发12",
+            Random: false,
+            Left: 50,
+            Top: 0,
+            Priority : 52,
+            ParentGroup: null,
+            Extended: false,
+            InheritColor: "HairFront",
+            Hide: ["HairFront"],
+            Layer: [               
+                {
+                    Name: "前",                  
+                    Priority: 52,
+                },
+                {
+                    Name: "后",                  
+                    Priority: 5,
+                }
+            ],
+        },
+        {
+            Name: "前发13",
+            Random: false,
+            Left: 50,
+            Top: 0,
+            Priority : 52,
+            ParentGroup: null,
+            Extended: false,
+            InheritColor: "HairFront",
+            Hide: ["HairFront"]
+        },
+        {
+            Name: "前发14",
+            Random: false,
+            Left: 50,
+            Top: 0,
+            Priority : 52,
+            ParentGroup: null,
+            Extended: false,
+            InheritColor: "HairFront",
+            Hide: ["HairFront"]
+        },
+        {
             Name: "卷发1",
             Random: false,
             Left: 0,
@@ -195,6 +238,9 @@ const translations = {
             前发9: "前发 9",
             前发10: "前发 10",
             前发11: "前发 11",
+            前发12: "前发 12",
+            前发13: "前发 13",
+            前发14: "前发 14",
             卷发1: "卷发 1",
             卷发2: "卷发 2",
             卷发3: "卷发 3",
@@ -213,6 +259,9 @@ const translations = {
             前发9: "HairFront 9",
             前发10: "HairFront 10",
             前发11: "HairFront 11",
+            前发12: "HairFront 12",
+            前发13: "HairFront 13",
+            前发14: "HairFront 14",
             卷发1: "Curly Hair 1",            
             卷发2: "Curly Hair 2",            
             卷发3: "Curly Hair 3"
