b2e5826b9b62a8a7b1ebbc13ce765e6bc020c4e4
emdsa2 <1260556316@qq.com>
2024-12-09 15:35:22 +0800

    添加 女仆围裙

---
+++ packages/mod/src/components/衣服和装饰/女仆围裙.js
@@ -0,0 +1,82 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "女仆围裙_Luzi",
+    Random: false,
+    Top: 0,
+    Left: 0,
+    Priority: 31,
+    DefaultColor :[
+        "Default",
+        "Default",
+        "Default",
+        "Default",
+        "#000000",
+    ],
+    PoseMapping: {
+        Hogtied: PoseType.HIDE,
+        AllFours: PoseType.HIDE,
+    },
+    Layer: [
+        {
+            Name: "肩带内",
+            ParentGroup : null,
+        },
+        {
+            Name: "肩带外",
+            ParentGroup : null,
+            PoseMapping: {
+                Yoked: "Yoked",
+                OverTheHead: "OverTheHead",
+                AllFours: PoseType.HIDE,
+                Hogtied: PoseType.HIDE,
+            },
+        },
+        {
+            Name: "裙",
+            PoseMapping: {
+                TapedHands: "TapedHands",
+                BackBoxTie: "BackElbowTouch",
+                BackCuffs: "BackElbowTouch",
+                BackElbowTouch: "BackElbowTouch",
+                OverTheHead: "BackElbowTouch",
+                Yoked: "BackElbowTouch",
+                AllFours: PoseType.HIDE,
+                Hogtied: PoseType.HIDE,
+            },
+        },
+        {
+            Name: "荷叶边",
+            PoseMapping: {
+                AllFours: PoseType.HIDE,
+                Hogtied: PoseType.HIDE,
+            },
+        },
+        {
+            Name: "线上",
+            PoseMapping: {
+                AllFours: PoseType.HIDE,
+                Hogtied: PoseType.HIDE,
+            },
+        },
+        {
+            Name: "线下",
+            ParentGroup : null,
+            PoseMapping: {
+                AllFours: PoseType.HIDE,
+                Hogtied: PoseType.HIDE,
+            },
+        },
+    ],
+};
+
+/** @type {Translation.Entry} */
+const translation = {
+    CN: "女仆围裙",
+    EN: "Maid's apron",
+};
+
+export default function () {
+    AssetManager.addAsset("Cloth", asset, undefined, translation);
+}
