6b09b77a7c5fbbeca6f325b90f940267467de29b
emdsa2 <1260556316@qq.com>
2024-12-11 09:09:17 +0800

    添加 裙子 披肩长 短

--- packages/mod/src/components/衣服和装饰/衣服/披肩.js
+++ packages/mod/src/components/衣服和装饰/衣服/披肩.js
@@ -1,52 +1,229 @@
 import AssetManager from "@mod-utils/AssetManager";
 
-/** @type {CustomAssetDefinition} */
-const asset = {
-    Name: "披肩",
-    Random: false,
-    Gender: "F",
-    Top: -110,
-    Left: 0,
-    Prerequisite: ["HasBreasts"],
-    Layer: [
+/** @type { CustomGroupedAssetDefinitions }} */
+const assets = {
+    Cloth: [
         {
-            Name: "Band",
-            Priority: 26,
-            PoseMapping: {
-                TapedHands: PoseType.DEFAULT,
-                Yoked: PoseType.DEFAULT,
-                OverTheHead: PoseType.DEFAULT,
-                BackBoxTie: PoseType.DEFAULT,
-                BackElbowTouch: PoseType.DEFAULT,
-                BackCuffs: PoseType.DEFAULT,
-                AllFours: PoseType.DEFAULT,
-                Hogtied: PoseType.DEFAULT,
-            },
+            Name: "披肩_Luzi",
+            Random: false,
+            Gender: "F",
+            Top: -110,
+            Left: 0,
+            Prerequisite: ["HasBreasts"],
+            Layer: [
+                {
+                    Name: "Band",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: PoseType.DEFAULT,
+                        OverTheHead: PoseType.DEFAULT,
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+                {
+                    Name: "Shawl",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: "Yoked",
+                        OverTheHead: "OverTheHead",
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+            ],
+        },
+    ],
+    ClothAccessory: [
+        {
+            Name: "披肩短_Luzi",
+            Random: false,
+            Gender: "F",
+            Top: 0,
+            Left: 0,
+            Prerequisite: ["HasBreasts"],
+            ParentGroup: "BodyUpper",
+            DefaultColor: [
+                "#490707",
+                "#490707",
+                "#FFFFFF",
+                "#FFFFFF",
+            ],
+            Layer: [
+                {
+                    Name: "衣左",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: "Yoked",
+                        OverTheHead: "OverTheHead",
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+                {
+                    Name: "衣右",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: "Yoked",
+                        OverTheHead: "OverTheHead",
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+                {
+                    Name: "绒左",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: "Yoked",
+                        OverTheHead: "OverTheHead",
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+                {
+                    Name: "绒右",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: "Yoked",
+                        OverTheHead: "OverTheHead",
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+            ],
         },
         {
-            Name: "Shawl",
-            Priority: 26,
-            PoseMapping: {
-                TapedHands: PoseType.DEFAULT,
-                Yoked: "Yoked",
-                OverTheHead: "OverTheHead",
-                BackBoxTie: PoseType.DEFAULT,
-                BackElbowTouch: PoseType.DEFAULT,
-                BackCuffs: PoseType.DEFAULT,
-                AllFours: PoseType.DEFAULT,
-                Hogtied: PoseType.DEFAULT,
-            },
+            Name: "披肩长_Luzi",
+            Random: false,
+            Gender: "F",
+            Top: 0,
+            Left: 0,
+            Prerequisite: ["HasBreasts"],
+            ParentGroup: "BodyUpper",
+            DefaultColor: [
+                "#490707",
+                "#490707",
+                "#FFFFFF",
+                "#FFFFFF",
+            ],
+            Layer: [
+                {
+                    Name: "衣左",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: "Yoked",
+                        OverTheHead: "OverTheHead",
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+                {
+                    Name: "衣右",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: "Yoked",
+                        OverTheHead: "OverTheHead",
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+                {
+                    Name: "绒左",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: "Yoked",
+                        OverTheHead: "OverTheHead",
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+                {
+                    Name: "绒右",
+                    Priority: 26,
+                    PoseMapping: {
+                        TapedHands: PoseType.DEFAULT,
+                        Yoked: "Yoked",
+                        OverTheHead: "OverTheHead",
+                        BackBoxTie: PoseType.DEFAULT,
+                        BackElbowTouch: PoseType.DEFAULT,
+                        BackCuffs: PoseType.DEFAULT,
+                        AllFours: PoseType.DEFAULT,
+                        Hogtied: PoseType.DEFAULT,
+                    },
+                },
+            ],
         },
     ],
 };
 
+/** @type { Translation.GroupedEntries } */
 const translations = {
-    CN: "披肩",
-    EN: "Shawl",
-    RU: "Палантин",
-    UA: "Палантин",
+    CN: {
+        Cloth: {
+            披肩_Luzi: "披肩",
+            披肩短_Luzi: "披肩短",
+            披肩长_Luzi: "披肩长",
+        },
+    },
+    EN: {
+        Cloth: {
+            披肩_Luzi: "Shawl",
+            披肩短_Luzi: "Short Shawl",
+            披肩长_Luzi: "Long Shawl",
+        },
+    },
+    RU: {
+        Cloth: {
+            披肩_Luzi: "Палантин",
+            披肩短_Luzi: "Короткий палантин",
+            披肩长_Luzi: "Длинный палантин",
+        },
+    },
+    UA: {
+        Cloth: {
+            披肩_Luzi: "Палантин",
+            披肩短_Luzi: "Короткий палантин",
+            披肩长_Luzi: "Довгий палантин",
+        },
+    },
 };
 
 export default function () {
-    AssetManager.addAsset("Cloth", asset, undefined, translations);
+    AssetManager.addGroupedAssets(assets, translations);
 }
---
+++ packages/mod/src/components/衣服和装饰/裙子.js
@@ -0,0 +1,72 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "裙子_Luzi",
+    Random: false,
+    Top: 0,
+    Left: 0,
+    Priority: 26,
+    DefaultColor: [
+        "#560E0E",
+        "#560E0E",
+        "#1F1F1F",
+    ],
+    PoseMapping: {
+        Hogtied: PoseType.HIDE,
+        AllFours: PoseType.HIDE,
+    },
+    Layer: [
+        {
+            Name: "上",
+            PoseMapping: {
+                AllFours: PoseType.HIDE,
+                Hogtied: PoseType.HIDE,
+            },
+        },
+        {
+            Name: "下",
+            PoseMapping: {
+                Kneel: "Kneel",
+                KneelingSpread: "KneelingSpread",
+                LegsClosed: PoseType.DEFAULT,
+                Spread: PoseType.DEFAULT,
+                AllFours: PoseType.HIDE,
+                Hogtied: PoseType.HIDE,
+            },
+        },
+        {
+            Name: "扣子",
+            ParentGroup: null,
+            PoseMapping: {
+                AllFours: PoseType.HIDE,
+                Hogtied: PoseType.HIDE,
+            },
+        }
+    ],
+};
+
+/** @type {Translation.Entry} */
+const translation = {
+    CN: "裙子",
+    EN: "Lace dress",
+};
+
+export default function () {
+
+    AssetManager.addAsset(
+        "ClothLower",
+        {
+            ...asset,
+            Left: {
+                [PoseType.DEFAULT]: 0,
+                KneelingSpread: 90,
+            },
+        },
+        undefined,
+        translation
+    );
+
+    AssetManager.addAsset("ClothLower", asset, undefined, translation);
+
+}
