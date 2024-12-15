377d3f6e76c2b0a0b5daae075fa3800d5f2b8fc6
XinLian132243 <XinLian132243@protonmail.com>
2024-12-16 02:18:16 +0800

    皮革中空短裙

---
+++ packages/mod/src/components/衣服和装饰/衣服/皮革中空短裙.js
@@ -0,0 +1,47 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+/** @type {CustomAssetDefinition} */
+const asset = {
+    Name: "皮革中空短裙",
+    Random: false,
+    Top: 0,
+    Left: 0,
+    DefaultColor: ["#Default", "Default"],
+    Expose: ["ItemVulva", "ItemVulvaPiercings", "ItemButt"],
+    PoseMapping: {
+        Hogtied: PoseType.HIDE,
+        AllFours: PoseType.HIDE,
+    },
+    ParentGroup : null,
+    Layer: [
+        {
+            Name: "底",
+            Priority: 26,
+        },
+        {
+            Name: "反光",
+            Priority: 26,
+        }
+    ],
+};
+
+/** @type {Translation.Entry} */
+const translation = {
+    CN: "皮革中空短裙",
+    EN: "Leather midriff skirt",
+};
+
+export default function () {
+    AssetManager.addAsset(
+        "ClothLower",
+        {
+            ...asset,
+            Left: {
+                [PoseType.DEFAULT]: 0,
+                KneelingSpread: 90,
+            },
+        },
+        undefined,
+        translation
+    );
+}
