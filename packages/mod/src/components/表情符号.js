170f1c1b9c3478daab7b3b513895573c55518575
dynilath <dynilath@qq.com>
2024-12-16 22:13:27 +0800

    feat: 新表情符号，旧表情符号整理

--- packages/mod/src/components/表情符号.js
+++ packages/mod/src/components/表情符号.js
@@ -1,8 +1,6 @@
 import AssetManager from "@mod-utils/AssetManager";
 
-const Emoticon内容 = [
-    "画稿子_Luzi",
-];
+const Emoticon内容 = ["画稿子_Luzi", "电影_Luzi", "VSCode_Luzi", "PS_Luzi"];
 
 export default function () {
     AssetManager.modifyAsset("Emoticon", "Emoticon", (group, asset) => {
