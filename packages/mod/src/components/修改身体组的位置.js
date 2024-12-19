e90fc9c42f3827994acce07d56c682a72d47cc8c
emdsa2 <1260556316@qq.com>
2024-12-19 13:29:56 +0800

    V2身体修改

---
+++ packages/mod/src/components/修改身体组的位置.js
@@ -0,0 +1,10 @@
+import AssetManager from "@mod-utils/AssetManager";
+
+
+export default function () {
+    // 生殖器
+    // AssetManager.modifyGroup("Pussy", (group) => { group.Top = 500 - 14; })
+    // AssetManager.modifyAsset("Pussy", "Penis", (group, asset) => {
+    //     asset.Top: 410 - 20;
+    // });
+}
