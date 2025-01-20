2503cd242f8a84574fe73cbabef200415e9b5cef
dynilath <dynilath@qq.com>
2025-01-21 07:08:30 +0800

    fix: 修正物品位置调整

--- packages/mod/src/components/修改身体组的位置.js
+++ packages/mod/src/components/修改身体组的位置.js
@@ -3,18 +3,29 @@ import { Tools } from "@mod-utils/Tools";
 
 export default function () {
     // 在加载完成之后，图层位置都直接写在图层属性里面了，所以这里直接调整图层位置
+
     AssetManager.modifyAssetLayers(
-        (asset) => asset.Group.Name == "Pussy",
+        (asset) => ["Pussy", "ItemVulva", "ItemVulvaPiercings", "ItemButt"].includes(asset.Group.Name),
         (asset, layer) => {
-            // 可以根据 asset.Name layer.Name 来判断是哪个图层
-            layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -16);
+            // topLeftAdjust 是相对于原位置调整，如果一个图层被调整了很多次，那么调整会叠加
+            // 所以这里集中所有的调整，避免重复调整
+
+            if (["FlatChastityCage", "PlasticChastityCage", "FuturisticTrainingBelt"].includes(asset.Name)) {
+                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -20);
+            } else {
+                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -16);
+            }
         }
     );
 
     AssetManager.modifyAssetLayers(
-        (asset) => asset.Name == "ItemVulva",
+        (asset) => asset.Group.Name == "Panties",
         (asset, layer) => {
-            layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -16);
+            if (asset.Name == "CockSock") {
+                layer.DrawingTop = Tools.topLeftOverride(layer.DrawingTop, 430);
+            } else if (asset.Name == "Jockstrap") {
+                layer.DrawingTop = Tools.topLeftOverride(layer.DrawingTop, 393);
+            }
         }
     );
 
@@ -30,63 +41,9 @@ export default function () {
     );
 
     AssetManager.modifyAssetLayers(
-        (asset) => asset.Group.Name == "ItemVulva",
-        (asset,layer)=>{
-            if(layer.Name == "FlatChastityCage")
-                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -20);
-            if(layer.Name == "PlasticChastityCage")
-                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -20);
-        }
-    );
-
-    AssetManager.modifyAssetLayers(
-        (asset) => asset.Group.Name == "ItemVulvaPiercings",
+        (asset) => asset.Name == "HarnessPanties1",
         (asset, layer) => {
-            layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -16);
+            layer.DrawingTop = Tools.topLeftOverride(layer.DrawingTop, 375);
         }
     );
-
-    AssetManager.modifyAssetLayers(
-        (asset) => asset.Group.Name == "ItemButt",
-        (asset, layer) => {
-            layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -16);
-        }
-    );
-    
-    AssetManager.modifyAssetLayers(
-        (asset) => asset.Group.Name == "ItemPelvis",
-        (asset,layer)=>{
-            if(layer.Name == "FuturisticTrainingBelt")
-                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -20);
-            if(layer.Name == "HarnessPanties1")
-                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -20);
-        }
-    );
-
-    AssetManager.modifyAssetLayers(
-        (asset) => asset.Group.Name == "Panties",
-        (asset,layer)=>{
-            if(layer.Name == "CockSock")
-                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -20);
-            if(layer.Name == "Jockstrap")
-                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -20);
-            if(layer.Name == "Jockstrap")
-                layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -20);
-        }
-    );
-    // TODO Saki救救
-    // AssetManager.modifyAssetLayers(
-    //     (asset) => asset.Group.Name == "ItemPelvis",
-    //     (asset,layer)=>{
-    //         if(layer.Name == "HarnessPanties1")
-    //             layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -40);
-    //     }
-    // );
-    // AssetManager.modifyAssetLayers(
-    //     (asset) => asset.Group.Name == "Panties",
-    //     (asset,layer)=>{
-    //         if(layer.Name == "HarnessPanties1")
-    //             layer.DrawingTop = Tools.topLeftAdjust(layer.DrawingTop, -40);
-    //     }
-    // );
-};
+}
