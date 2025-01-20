950b2c044ec12e72aaed16fbed585356cee5943b
dynilath <dynilath@qq.com>
2025-01-21 07:17:39 +0800

    chore: 增加主要版本，调整描述，beta版本的版本号区别

--- packages/rollup.tools.js
+++ packages/rollup.tools.js
@@ -185,7 +185,7 @@ function createRollupConfig(curDir, baseURL, modInfo, rollupSetting, betaFlag = 
 
         __mod_name__: `"${modInfo.name}"`,
         __mod_full_name__: `"${modInfo.fullName}"`,
-        __mod_version__: `"${modInfo.version}"`,
+        __mod_version__: `"${modInfo.version}-${betaFlag ? "beta" : ""}"`,
         __mod_repo__: modInfo.repo ? `"${modInfo.repo}"` : "undefined",
         __mod_asset_overrides__: assetMapping,
         __mod_base_url__: `"${baseURL}"`,
