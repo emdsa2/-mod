204c3695d19ec75351fbe224e878324448fe3824
dynilath <dynilath@qq.com>
2025-01-21 19:11:00 +0800

    feat: 细化生化人身体

--- packages/mod/src/components/衣服和装饰/动物身体/生化人体.js
+++ packages/mod/src/components/衣服和装饰/动物身体/生化人体.js
@@ -1,6 +1,100 @@
 import AssetManager from "@mod-utils/AssetManager";
 import ModManager from "@mod-utils/ModManager";
 
+const NamePrefix = ((prefix) => prefix + Math.random().toString(16).substring(2))("生化人体");
+
+/**
+ *
+ * @param {Character} C
+ * @param {Asset} asset
+ * @param {Number} width
+ * @param {Number} height
+ * @param {string} suffix
+ * @returns
+ */
+function customTempCanvas(C, asset, width, height, suffix) {
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("name", `${NamePrefix}__${C.CharacterID}__${asset.Group.Name}__${asset.Name}__${suffix}`);
+    canvas.width = width;
+    canvas.height = height;
+    return canvas;
+}
+
+/**
+ * 绘制其他身体部位
+ * @param {Character} C
+ * @param {Asset} TempCanvasAsset
+ * @param {CustomGroupName[]} DrawGroupNames
+ */
+function partialDraw(C, TempCanvasAsset, DrawGroupNames) {
+    const oldDrawCanvas = GLDrawCanvas;
+
+    GLDrawCanvas = customTempCanvas(C, TempCanvasAsset, CanvasDrawWidth, CanvasDrawHeight, "GLDrawCanvas");
+    /** @type {Character} */
+    const copyC = {
+        ...C,
+        CharacterID: "npc-partial-draw",
+        Appearance: C.Appearance.filter((a) => DrawGroupNames.includes(a.Asset.Group.Name)),
+    };
+    copyC.Canvas = customTempCanvas(C, TempCanvasAsset, CanvasDrawWidth, CanvasDrawHeight, "Canvas");
+    copyC.CanvasBlink = customTempCanvas(C, TempCanvasAsset, CanvasDrawWidth, CanvasDrawHeight, "CanvasBlink");
+    copyC.DrawAppearance = AppearanceItemParse(CharacterAppearanceStringify(copyC));
+    copyC.AppearanceLayers = CharacterAppearanceSortLayers(copyC);
+    copyC.Appearance = C.Appearance;
+
+    CharacterAppearanceBuildCanvas(copyC);
+
+    GLDrawCanvas = oldDrawCanvas;
+    return { Canvas: copyC.Canvas, CanvasBlink: copyC.CanvasBlink };
+}
+
+/**
+ * @param {Character} C
+ * @param {Asset} Asset
+ * @param {string} LayerName
+ * @param {AssetPoseName} Pose
+ * @param {string} ParentAssetName
+ * @param {string} [OverrideName]
+ */
+function getMaskURL(C, Asset, LayerName, Pose, ParentAssetName, OverrideName) {
+    const layer = Asset.Layer.find((l) => l.Name === LayerName);
+
+    let poseSegment = layer.PoseMapping[Pose];
+    switch (poseSegment) {
+        case PoseType.HIDE:
+        case PoseType.DEFAULT:
+        case undefined:
+            poseSegment = "";
+            break;
+        default:
+            poseSegment += "/";
+            break;
+    }
+
+    const urlParts = [asset.Name, ParentAssetName, OverrideName ?? LayerName].filter((c) => c);
+
+    return `Assets/${Asset.Group.Family}/${Asset.Group.Name}/${poseSegment}${urlParts.join("_")}.png`;
+}
+
+/** @type {ExtendedItemCallbacks.AfterDraw<{}>} */
+function androidDraw({ C, A, X, Y, Pose, drawCanvas, drawCanvasBlink, AlphaMasks, L, G }) {
+    if (L === "上身遮罩" || L === "下身遮罩") {
+        const { Canvas, CanvasBlink } = partialDraw(
+            C,
+            A,
+            L === "上身遮罩" ? ["BodyUpper", "ArmsLeft", "ArmsRight"] : ["BodyLower"]
+        );
+
+        const maskURL = getMaskURL(C, A, L, Pose, G, "身体遮罩");
+
+        DrawImageEx(maskURL, Canvas.getContext("2d"), X, Y, { BlendingMode: "destination-out" });
+        DrawImageEx(maskURL, CanvasBlink.getContext("2d"), X, Y, { BlendingMode: "destination-out" });
+
+        drawCanvas(Canvas, 0, 0, AlphaMasks);
+        drawCanvasBlink(CanvasBlink, 0, 0, AlphaMasks);
+    }
+}
+
 /** @type {CustomAssetDefinition} */
 const asset = {
     Name: "生化人体",
@@ -8,71 +102,59 @@ const asset = {
     Top: 0,
     Left: 0,
     Priority: 9,
+    Gender: "F",
     Expose: ["ItemVulva", "ItemVulvaPiercings", "ItemButt"],
-    DefaultColor: ["Default"],
+    DefaultColor: ["Default", "Default", "Default"],
     PoseMapping: {
         Hogtied: PoseType.HIDE,
         AllFours: PoseType.HIDE,
     },
+    DynamicAfterDraw: true,
     ParentGroup: null,
-    AllowColorize: false,
+    AllowColorize: true,
+    Hide: ["BodyUpper", "BodyLower", "ArmsLeft", "ArmsRight", "Nipples"],
     Layer: [
         {
             Name: "骨架",
+            PoseMapping: {
+                Kneel: "LegsClosed",
+                LegsClosed: "LegsClosed",
+                Spread: "Spread",
+                KneelingSpread: "KneelingSpread",
+                Hogtied: PoseType.HIDE,
+                AllFours: PoseType.HIDE,
+            },
+            Priority: 5,
         },
         {
             Name: "阴道",
+            Priority: 6,
         },
         {
-            Name: "擦除身体",
-            BlendingMode: "destination-out",
+            Name: "上身遮罩",
+            ParentGroup: "BodyUpper",
+            Priority: 7,
+            AllowColorize: false,
+            HasImage: false,
+        },
+        {
+            Name: "下身遮罩",
+            ParentGroup: "BodyLower",
+            Priority: 7,
+            AllowColorize: false,
+            HasImage: false,
         },
         {
             Name: "网格",
+            Priority: 9,
         },
     ],
 };
 
-/**
- * 增强 BlendingMode 支持
- * @param {WebGL2RenderingContext} gl
- * @param {GlobalCompositeOperation} blendingMode
- */
-function blendingModeExt(gl, blendingMode) {
-    switch (blendingMode) {
-        case "source-over":
-            gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
-            break;
-        case "source-in":
-            gl.blendFuncSeparate(gl.DST_ALPHA, gl.ZERO, gl.ZERO, gl.ONE);
-            break;
-        case "source-out":
-            gl.blendFuncSeparate(gl.ZERO, gl.ONE_MINUS_SRC_ALPHA, gl.ZERO, gl.ONE_MINUS_SRC_ALPHA);
-            break;
-        case "source-atop":
-            gl.blendFuncSeparate(gl.DST_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ZERO, gl.ONE);
-            break;
-        case "destination-over":
-            gl.blendFuncSeparate(gl.ONE_MINUS_DST_ALPHA, gl.ONE, gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
-            break;
-        case "destination-in":
-            gl.blendFuncSeparate(gl.ZERO, gl.SRC_ALPHA, gl.DST_ALPHA, gl.SRC_ALPHA);
-            break;
-        case "destination-out":
-            gl.blendFuncSeparate(gl.ZERO, gl.ONE_MINUS_SRC_ALPHA, gl.ZERO, gl.ONE_MINUS_SRC_ALPHA);
-            break;
-        case "destination-atop":
-            gl.blendFuncSeparate(gl.ONE_MINUS_DST_ALPHA, gl.SRC_ALPHA, gl.ZERO, gl.ONE);
-            break;
-    }
-}
-
 export default function () {
-    const func = ModManager.randomGlobalFunction("blendingModeExt", blendingModeExt);
+    const assetGroup = "动物身体_Luzi";
 
-    ModManager.patchFunction("GLDrawImage", {
-        [`gl.bindBuffer(gl.ARRAY_BUFFER, program.position_buffer);`]: `${func}(gl,blendingMode); gl.bindBuffer(gl.ARRAY_BUFFER, program.position_buffer);`,
-    });
+    ModManager.globalFunction(`Assets${assetGroup}${asset.Name}AfterDraw`, androidDraw);
 
-    AssetManager.addAsset("动物身体_Luzi", asset);
+    AssetManager.addAsset(assetGroup, asset);
 }
