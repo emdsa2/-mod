d223da9f62d5cedf709c36165ee936ee46dfa4f8
emdsa2 <1260556316@qq.com>
2025-02-11 22:53:12 +0800

    修改

--- packages/mod/src/components/衣服和装饰/中式元素.js
+++ packages/mod/src/components/衣服和装饰/中式元素.js
@@ -49,12 +49,8 @@ const assets = {
             Left: 0,
             Priority: 55,
             Layer: [
-                {
-                    Name: "左",
-                },
-                {
-                    Name: "右",
-                },
+                { Name: "左", },
+                { Name: "右", },
             ],
         },
         {
@@ -64,12 +60,32 @@ const assets = {
             Left: 0,
             Priority: 40,
             Layer: [
-                {
-                    Name: "左",
-                },
-                {
-                    Name: "右",
-                },
+                { Name: "左", },
+                { Name: "右", },
+            ],
+        },
+    ],
+    HairAccessory2: [
+        {
+            Name: "茉莉花钿1",
+            Random: false,
+            Top: 0,
+            Left: 0,
+            Priority: 55,
+            Layer: [
+                { Name: "左", },
+                { Name: "右", },
+            ],
+        },
+        {
+            Name: "茉莉花钿2",
+            Random: false,
+            Top: 0,
+            Left: 0,
+            Priority: 40,
+            Layer: [
+                { Name: "左", },
+                { Name: "右", },
             ],
         },
     ],
@@ -80,6 +96,12 @@ const translations = {
     CN: {
         Cloth: {
             假领子_Luzi: "假领子",
+        },
+        HairAccessory1: {
+            茉莉花钿1: "茉莉花钿 1",
+            茉莉花钿2: "茉莉花钿 2",
+        },
+        HairAccessory2: {
             茉莉花钿1: "茉莉花钿 1",
             茉莉花钿2: "茉莉花钿 2",
         },
@@ -87,13 +109,27 @@ const translations = {
     EN: {
         Cloth: {
             假领子_Luzi: "Fake Collar",
+        },
+        HairAccessory1: {
             茉莉花钿1: "Jasmine Hairpin 1",
             茉莉花钿2: "Jasmine Hairpin 2",
         },
+        HairAccessory2: {
+            茉莉花钿1: "Jasmine Hairpin 1",
+            茉莉花钿2: "Jasmine Hairpin 2",
+        },
+
     },
     RU: {
         Cloth: {
             假领子_Luzi: "Поддельный воротник",
+
+        },
+        HairAccessory1: {
+            茉莉花钿1: "Жасминовая шпилька 1",
+            茉莉花钿2: "Жасминовая шпилька 2",
+        },
+        HairAccessory2: {
             茉莉花钿1: "Жасминовая шпилька 1",
             茉莉花钿2: "Жасминовая шпилька 2",
         },
